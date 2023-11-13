b911b0d72f532a65a679a8adf34c1f9008c76649
codexindia <sudiptobain8@gmail.com>
2023-11-13 17:53:43 +0530

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -24,5 +25,12 @@ class User extends Authenticatable
         'country_code',
         'profile_pic'
     ];
-
+    public function getProfilePicAttribute($value)
+    {
+        if(!$value == null)
+        {
+            return asset(Storage::url($value));
+        }
+       return null;
+    }
 }

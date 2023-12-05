3b9e7c188cba224301dacfe42584b91b793b768d
codexindia <sudiptobain8@gmail.com>
2023-12-05 11:09:08 +0530

    fff

--- app/Models/UploadedVideos.php
+++ app/Models/UploadedVideos.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class UploadedVideos extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+    public function getThumbnailAttribute($value)
+    {
+        if(!$value == null)
+        {
+            return asset(Storage::url($value));
+        }
+        return null;
+    }
 }

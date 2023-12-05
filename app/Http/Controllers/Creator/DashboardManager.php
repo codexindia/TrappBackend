3140ad2b06434575c8544990847acf1c36a19d23
codexindia <sudiptobain8@gmail.com>
2023-12-05 13:12:40 +0530

    sa

--- app/Http/Controllers/Creator/DashboardManager.php
+++ app/Http/Controllers/Creator/DashboardManager.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers\Creator;
 
 use App\Http\Controllers\Controller;
+use App\Models\UploadedVideos;
 use Illuminate\Http\Request;
 
 class DashboardManager extends Controller
 {
     public function get_counts(Request $request)
     {
+        $videos = UploadedVideos::where([
+            'creator_id'=> $request->user()->id,
+            'video_type'=>'simple'
+        ])->count();
+        $lives =UploadedVideos::where([
+            'creator_id'=> $request->user()->id,
+            'video_type'=>'live'
+        ])->count();
         $counts = array();
-        $counts["revenue"] = 50;
+        $counts["revenue"] = 0;
         $counts["followers"] = 50;
-        $counts["videos"] = 0;
-        $counts["live"] = 50;
+        $counts["videos"] = $videos;
+        $counts["live"] = $lives;
         //$counts = json_encode($counts);
         return response()->json([
             'status' => true,

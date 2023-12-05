bf3010209c45381bc5392f0cff61b3948a34069f
codexindia <sudiptobain8@gmail.com>
2023-12-05 16:17:32 +0530

    pupda

--- app/Http/Controllers/Admin/DashboardManager.php
+++ app/Http/Controllers/Admin/DashboardManager.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use App\Models\UploadedVideos;
+use App\Models\Creator;
 class DashboardManager extends Controller
 {
     public function get_counts(Request $request)
     {
         $counts = array();
-        $counts["users"] = 50;
-        $counts["creators"] = 50;
-        $counts["videos"] = 0;
-        $counts["live"] = 50;
+        $counts["users"] = User::count();
+        $counts["creators"] = Creator::count();
+        $counts["videos"] = UploadedVideos::where('video_type','normal')->count();
+        $counts["live"] = UploadedVideos::where('video_type','live')->count();
         //$counts = json_encode($counts);
         return response()->json([
             'status' => true,

b2774a4c35194043c8e89ab185bf104b681ccd63
codexindia <sudiptobain8@gmail.com>
2024-01-11 16:33:38 +0530

    dd

--- app/Http/Controllers/Api/AuthManager.php
+++ app/Http/Controllers/Api/AuthManager.php
@@ -179,9 +179,9 @@ class AuthManager extends Controller
   
         try {
   
-            $account_sid = getenv("TWILIO_SID");
-            $auth_token = getenv("TWILIO_TOKEN");
-            $twilio_number = getenv("TWILIO_FROM");
+            $account_sid = env("TWILIO_SID");
+            $auth_token = env("TWILIO_TOKEN");
+            $twilio_number = env("TWILIO_FROM");
   
             $client = new Twilio($account_sid, $auth_token);
             $client->messages->create($receiverNumber, [

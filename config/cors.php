d667e39c1f83a8566ee48dbd84ca1abdcca77d6e
codexindia <sudiptobain8@gmail.com>
2024-07-06 23:27:19 +0530

    asdads

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*','creator/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://trapp-creator-panel.vercel.app'], //add your allowed origins
+    'allowed_origins' => ['*as'], //add your allowed origins
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

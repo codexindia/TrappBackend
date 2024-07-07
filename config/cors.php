aab62b46e96a0bde7b0eac05c422f274d850fab7
codexindia <sudiptobain8@gmail.com>
2024-07-07 15:15:19 +0530

    sd

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*','creator/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], //add your allowed origins
+    //'allowed_origins' => ['*'], //add your allowed origins
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

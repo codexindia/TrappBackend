b94e4abd9f5e2db0403527ec7b47f2214ed7ba93
codexindia <sudiptobain8@gmail.com>
2024-07-07 14:17:39 +0530

    d

--- app/Http/Middleware/CheckSecretToken.php
+++ app/Http/Middleware/CheckSecretToken.php
@@ -15,7 +15,8 @@ class CheckSecretToken
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $request->headers->set('Accept', 'application/json');
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
         $request->headers->set('secret', 'hellothisisocdexindia');
         if ($request->header('secret') == 'hellothisisocdexindia') {
              return $next($request);
